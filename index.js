4c1d482970a6b444bdca8aa648d94a1c1f73dce3
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2017-03-01 15:53:05 -0500

    fix typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   init: function (app, mongoose, passport, secret, uri) {
     global.mongoose = mongoose;
-    global.user = require ('./models/User');
+    global.User = require ('./models/User');
     global.passport = passport;
     let session = require ('express-session');
     let MongoStore = require ('connect-mongo') (session);
